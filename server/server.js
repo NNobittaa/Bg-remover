e5e1ea4391d82ecb749607e53c1a1dca93cc8bec
NNobittaa <rajat28012006@gmail.com>
2025-10-31 14:40:41 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,27 +1,17 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import connectDB from './configs/mongodb.js'
+import connectDB from '../configs/mongodb.js'
 
 const app = express()
-const PORT = process.env.PORT || 3000
-
 app.use(express.json())
 app.use(cors())
 
-const startServer = async () => {
-  try {
-    await connectDB()
-    console.log('✅ MongoDB Connected')
+app.get('/', (req, res) => {
+  res.send('API Working ✅')
+})
 
-    app.get('/', (req, res) => res.send('API Working'))
+// Connect to MongoDB (on first cold start)
+await connectDB()
 
-    app.listen(PORT, () =>
-      console.log(`🚀 Server running at http://localhost:${PORT}/`)
-    )
-  } catch (err) {
-    console.error('❌ Server error:', err.message)
-  }
-}
-
-startServer()
+export default app
