228598dcad55a95773949b5973e41a9855f7511f
NNobittaa <rajat28012006@gmail.com>
2025-11-01 22:42:40 +0530

    server.j

--- server/api/index.js
+++ server/api/index.js
@@ -1,17 +1,16 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import connectDB from './configs/mongodb.js'
+import connectDB from '../configs/mongodb.js'
 
-//App config S
-const PORT = process.env.PORT || 4000
 const app = express()
-await connectDB()
-
-//Initialize Middlewares
 app.use(express.json())
 app.use(cors())
 
-//API Routes
-app.get('/', (req, res)=>res.send("API working"))
-app.listen(PORT, ()=> console.log("Server running on PORT http://localhost:"+PORT))
+await connectDB()
+
+app.get('/', (req, res) => {
+  res.send('API working ✅ (from Vercel)')
+})
+
+export default app
