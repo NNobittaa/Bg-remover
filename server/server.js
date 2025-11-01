61886d5ef86270f21052486c4d8cb138aaae43a6
NNobittaa <rajat28012006@gmail.com>
2025-11-01 21:44:08 +0530

    new server first commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+//App config 
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+//Initialize Middlewares
+app.use(express.json())
+app.use(cors())
+
+//API Routes 
+app.get('/', (req, res)=>res.send("API working"))
+app.listen(PORT, ()=> console.log("Server running on PORT http://localhost:"+PORT))
