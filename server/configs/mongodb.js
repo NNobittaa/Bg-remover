7d6f74280fe600fc69c1de2b4756e8a6e45ae426
NNobittaa <rajat28012006@gmail.com>
2025-11-12 19:29:49 +0530

    Server update

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -7,6 +7,7 @@ const connectDB = async()=>{
     })
 
     await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
+    
 }
 
 export default connectDB
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { connect } from 'mongoose'
+import userRouter from './routes/userRoutes.js'
 
 // Configure app
 const PORT = process.env.PORT || 4000
@@ -12,6 +13,7 @@ await connectDB()
 // Initialize middleware 
 app.use(cors())
 app.use(express.json())
+app.use('/api/user', userRouter)
 
 //API Routes 
 app.get('/',(req, res)=>res.send("API Working"))
