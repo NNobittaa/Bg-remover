cb934eec17e66da9faf4543f52cae6b10fab55e5
NNobittaa <rajat28012006@gmail.com>
2025-11-12 20:00:54 +0530

    appciontext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,9 +15,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <AppContextProvider>
+    {/* <AppContextProvider> */}
     <App />
-    </AppContextProvider>
+    {/* </AppContextProvider> */}
     </ClerkProvider>
   </BrowserRouter>,
 )
