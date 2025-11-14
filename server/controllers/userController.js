81f504fafe92689b53dd28375f768b83f1489744
NNobittaa <rajat28012006@gmail.com>
2025-11-14 22:16:46 +0530

    server

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,10 +11,12 @@ export default async function clerkWebhooks(req, res) {
                 "svix-timestamp": req.headers["svix-timestamp"],
                 "svix-signature": req.headers["svix-signature"],
             }
-        );
+          );
+          console.log(req.body)
+          
+          console.log("Webhook Verified:", evt.type);
+          res.json({ success: true });
 
-        console.log("Webhook Verified:", evt.type);
-        res.json({ success: true });
     } catch (err) {
         console.error("Webhook Error:", err.message);
         res.status(400).json({ success: false, message: err.message });
