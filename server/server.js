5466957a2261ba27f4d795cfd29979e3aafafb01
NNobittaa <rajat28012006@gmail.com>
2025-11-20 18:29:53 +0530

    final changes

--- server/server.js
+++ server/server.js
@@ -10,7 +10,12 @@ const app = express()
 
 await connectDB()
 
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}));
+
 
 // RAW BODY for Clerk
 app.use('/api/user/webhooks', express.raw({ type: 'application/json' }))
