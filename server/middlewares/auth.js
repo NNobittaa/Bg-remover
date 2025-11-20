58566a2d568e16588a6b0167a00b58a9e8c3cdaa
NNobittaa <rajat28012006@gmail.com>
2025-11-20 23:46:42 +0530

    e

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -24,7 +24,7 @@ const authUser = async(req, res, next)=>{
         next()  
     }
     catch(error){
-        console.log(error.message)
+        console.log(error)
         res.json({success:false, message:error.message})
 
     }
