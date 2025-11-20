51394eaf3e81823ba93ee8f0fbc6cb6c969075b0
NNobittaa <rajat28012006@gmail.com>
2025-11-20 19:04:21 +0530

    port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import connectDB from './configs/mongodb.js'
 import userRouter from './routes/userRoutes.js'
 import imageRouter from './routes/imageRoutes.js'
 
-// const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 const app = express()
 
 await connectDB()
@@ -25,4 +25,4 @@ app.use('/api/image', imageRouter)
 // Root route
 app.get('/', (req, res) => res.send("API Working"))
 
-// app.listen(PORT, () => console.log('Server running on port: ' + PORT))
+app.listen(PORT, () => console.log('Server running on port: ' + PORT))
